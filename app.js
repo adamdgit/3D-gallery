3cf39ff1e7a29d412f1cb0d8966369d8e555c7ed
adamdgit <adam_d@outlook.com>
2023-02-19 17:02:55 +1100

    store prev rotation, so gallery pos doesn't reset

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 const perspective = document.querySelector('.perspective');
 const wrapper = document.querySelector('.wrap')
 
-const sensitivity = 5
+// lower rotates more quickly
+const sensitivity = 4;
+// save previous rotation
+let prevdeg = 0;
+// current rotation
+let deg = 0;
 
 perspective.addEventListener('pointerdown', handlePointerDown);
 
 function handlePointerDown(e) {
+  // cancel pointerdown event
   e.preventDefault();
+  // get mouse pos on click
+  const x = e.clientX;
+
+  // rotate the gallery
   perspective.addEventListener('pointermove', handlePointerMove)
+
+  // transform rotation of wrapper on mouse move
+  function handlePointerMove(e) {
+
+    function rotateGallery(e) {
+      deg = (e.clientX - x) / sensitivity;
+      console.log(prevdeg)
+      wrapper.style.transform = `translateZ(50vw) rotateY(${deg + prevdeg}deg) rotate(-3deg)`;
+    }
+    // 
+    window.requestAnimationFrame(() => {
+      rotateGallery(e);
+    });
+  };
+
+  // save current rotation value
+  prevdeg += deg;
+
   perspective.addEventListener('pointerup', handlePointerUp)
-};
-
-// transform rotation of wrapper 
-function handlePointerMove(e) {
-  console.log(e)
-
-  function rotateGallery(element, y, x) {
-    let box = element.getBoundingClientRect();
-    let xAxis = -(y - box.y - (box.height / 2)) / sensitivity;
-    let yAxis = (x - box.x - (box.width / 2)) / sensitivity;
-    let deg = (xAxis + yAxis) / 4
-  
-    wrapper.style.transform = `translateZ(520px) rotateY(${deg}deg)`;
-  }
-
-  window.requestAnimationFrame(() => {
-    rotateGallery(wrapper, e.clientY, e.clientX);
-  });
-};
-
-// removed pointermove event listener on pointerup
-function handlePointerUp(e) {
-  perspective.removeEventListener('pointermove', handlePointerMove)
+  // removed pointermove event listener on pointerup
+  function handlePointerUp(e) {
+    perspective.removeEventListener('pointermove', handlePointerMove)
+  };
 };
